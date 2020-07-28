9ba24ab20b74a6d83795f79b9456025736fca7e6
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2020-07-28 17:44:22 -0300

    Commit inicial do projeto baseado na imersão de React da Alura (07/2020)

---
+++ src/components/Menu/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png';
+import './Menu.css';
+import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+
+function Menu() {
+    return (
+        <nav className = "Menu">
+            <a href="/">
+                <img className = "Logo" src={Logo} alt="Aluraflix logo" />
+            </a>
+
+            <ButtonLink className="ButtonLink" href="/">
+                Novo Vídeo
+            </ButtonLink>
+            {/* <Button as="a" className="ButtonLink" href="/">
+                Novo Vídeo
+            </Button> */}
+        </nav>
+    );
+}
+
+export default Menu;
